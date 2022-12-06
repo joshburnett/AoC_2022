7f789a1d8abce50debe0e3b82e19dc576b33979e
Josh Burnett <jburnett1@werfen.com>
2022-12-06 08:23:57 -0500

    Completed day 6

---
+++ day06/day06.py
@@ -0,0 +1,75 @@
+# Imports for timing functions
+try:
+    from time import perf_counter_ns as monotonic_ns
+    # from day01_cpython import total_calories_by_elf, part1, part2
+    # from day01_circuitpython import total_calories_by_elf, part1, part2, python_runtime
+    # python_runtime['name'] = 'CPython'
+
+except ImportError:
+    try:
+        from time import monotonic_ns
+        # from day01_circuitpython import total_calories_by_elf, part1, part2, python_runtime
+        # python_runtime['name'] = 'CircuitPython'
+    except ImportError:
+        from time import ticks_us
+        # from day01_circuitpython import total_calories_by_elf, part1, part2, python_runtime
+        # python_runtime['name'] = 'MicroPython'
+
+        def monotonic_ns():
+            return ticks_us()*1000
+
+
+#%% Imports for solving puzzle
+from collections import deque
+
+#%% Part 1, test
+q = deque(maxlen=4)
+
+with open('day06_test.txt', 'r') as f:
+    while chars := f.readline():
+        q.clear()
+        for n, char in enumerate(chars):
+            q.append(char)
+            if len(set(q)) == 4:
+                break
+
+        print(f'n: {n + 1}    {chars.strip()}')
+
+
+# %% Part 2, test
+q = deque(maxlen=14)
+
+with open('day06_test.txt', 'r') as f:
+    while chars := f.readline():
+        q.clear()
+        for n, char in enumerate(chars):
+            q.append(char)
+            if len(set(q)) == 14:
+                print(''.join(q))
+                break
+
+        print(f'n: {n + 1}    {chars.strip()}\n')
+
+# %% Part 1
+q = deque(maxlen=4)
+
+with open('day06.txt', 'r') as f:
+    chars = f.readline()
+    for n, char in enumerate(chars):
+        q.append(char)
+        if len(set(q)) == 4:
+            break
+
+    print(f'n: {n + 1}')
+
+# %% Part 2
+q = deque(maxlen=14)
+
+with open('day06.txt', 'r') as f:
+    chars = f.readline()
+    for n, char in enumerate(chars):
+        q.append(char)
+        if len(set(q)) == 14:
+            break
+
+    print(f'n: {n + 1}')
